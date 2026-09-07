d4a2a0a85321c927c3149f8e3cca24c8c0cee609
kurilo4ka <kirmenkim007@gmail.com>
2026-09-07 15:07:36 +0300

    initial commit

---
+++ src/main/java/com/manager/tab/tabmanager/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.manager.tab.tabmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/")
+    public String index(){
+        return "Users will be here";
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/com/manager/tab/tabmanager/TabmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.tab.tabmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
