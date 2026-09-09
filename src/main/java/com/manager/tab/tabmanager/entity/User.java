bcf1891e27eafa679bdfbd41db81d45c128627f1
nikest <nikest@taltech.ee>
2026-09-09 16:18:07 +0300

    cleaner code (Lombok)

--- src/main/java/com/manager/tab/tabmanager/entity/User.java
+++ src/main/java/com/manager/tab/tabmanager/entity/User.java
@@ -1,6 +1,11 @@
 package com.manager.tab.tabmanager.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
 
 @Entity
 @Table(name="users",
@@ -8,6 +13,12 @@ import jakarta.persistence.*;
             @UniqueConstraint(columnNames = "username"),
             @UniqueConstraint(columnNames = "email")
     })
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -21,45 +32,4 @@ public class User {
 
     @Column(name="password", length = 20, nullable = false)
     private String password;
-
-    public User(){
-    }
-
-    public User(String username, String email, String password){
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
